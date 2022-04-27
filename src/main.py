d94de3e5748e284c0c601aba4e43afdd96ecb948
atvc0202 <atulvc2001@gmail.com>
2022-04-27 22:15:52 +0530

    trying out some stuff

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
 import xlsxwriter
 
-def Accnt():
-    bookName = xlsxwriter.Workbook('AccountingRam.xlsx')
+class theBook:
+
+    content = ["Client name","Advance","Balance","Total"]
+
+    def Accnt(self,clnm):
+        bookName = xlsxwriter.Workbook('AccountingsRam.xlsx')
+        bookSheet = bookName.add_worksheet()
+
+        row = 0
+        coloumn = 0
+
+        for item in content:
+
+            bookSheet.write(row,coloumn,item)
+            row += 1
+
+    def clientname(clntname)
+
+if __name__ == "__main__":
+    
+    theBook.run()
+    bookName = xlsxwriter.Workbook('AccountingsRam.xlsx')
     bookSheet = bookName.add_worksheet()
-
-
-
-
-
-
-
-
-if __name__ = "__main__":
+   
+    clntname = str(input("Please Enter your Client's name"))
